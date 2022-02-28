4adf80fb49afc3d1663134f54307f6033a5f1b31
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2022-02-28 15:33:40 +0330

    working on the review api

--- app/Http/Controllers/v1/ReviewController.php
+++ app/Http/Controllers/v1/ReviewController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\v1\Review as ReviewResource;
 use App\Http\Resources\v1\ReviewCollection;
+use Illuminate\Http\Request;
 use App\Models\Review;
 
 class ReviewController extends Controller
@@ -12,5 +14,26 @@ class ReviewController extends Controller
     {
         $product = Review::all();
         return new ReviewCollection($product);
+    }
+
+    public function review(Request $request)
+    {
+        $this->validate($request, [
+            'product_id' => 'required|exists:products,id',
+            'rating' => 'required|integer',
+            'reviewCount' => 'required|integer',
+            'message' => 'required|string|min:10|max:255',
+            'subject' => 'required|string|max:10'
+        ]);
+
+        $review = Review::create([
+            'product_id' => $request->input('product_id'),
+            'rating' => $request->input('rating'),
+            'reviewCount' => $request->input('reviewCount'),
+            'message' => $request->input('message'),
+            'subject' => $request->input('subject')
+        ]);
+
+        return new ReviewResource($review);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@
 $router->group(['prefix' => 'v1', 'namespace' => 'v1'], function () use ($router) {
     $router->get('products', 'ProductController@index');
     $router->get('reviews', 'ReviewController@index');
+    $router->post('review', 'ReviewController@review');
     $router->post('login', 'UserController@login');
     $router->post('register', 'UserController@register');
     $router->group(['middleware' => 'auth'], function () use ($router) {
