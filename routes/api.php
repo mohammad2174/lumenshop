f6c14abb9bcc1fab2b46327dcffbc2b8d45bb61e
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2022-03-16 14:49:25 +0330

    creating quantity route

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ $router->group(['prefix' => 'v1', 'namespace' => 'v1'], function () use ($router
     $router->get('checkouts', 'CheckoutController@index');
     $router->post('checkout', 'CheckoutController@checkout');
     $router->get('deletecheckout', 'CheckoutController@delete');
+    $router->get('quantity', 'CheckoutController@quantity');
     $router->get('orders', 'OrderController@index');
     $router->post('order', 'OrderController@order');
     $router->post('login', 'UserController@login');
